8b68584ada6b76260f29f59ad7f74caebadee9ea
Maria Lishchinskaya <mlishchinskaya@gmail.com>
2019-05-01 23:11:49 +0300

    added services component

---
+++ js/list-item.js
@@ -0,0 +1,22 @@
+const services = document.querySelector('.services');
+const arrayOfItems = services.querySelector('.services__list').querySelectorAll('.list__item');
+let activeService = 1;
+
+arrayOfItems[activeService].classList.add('active');
+
+const toggleActiveService = () => {
+  arrayOfItems.forEach(el => el.classList.remove('active'));
+  arrayOfItems[activeService].classList.add('active');
+};
+
+services.querySelector('.button_prev').addEventListener('click', () => {
+  --activeService;
+  if (activeService < 0) activeService = arrayOfItems.length -1;
+  toggleActiveService();
+}, false);
+
+services.querySelector('.button_next').addEventListener('click', () => {
+  ++activeService;
+  if (activeService > (arrayOfItems.length - 1)) activeService = 0;
+  toggleActiveService()
+}, false);
