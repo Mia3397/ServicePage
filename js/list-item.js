ef183bd26d50000a81628cf1d920ee78078ba60e
Maria Lishchinskaya <mlishchinskaya@gmail.com>
2019-05-03 22:09:33 +0300

    renamed classes, deleted selectors combine

--- js/list-item.js
+++ js/list-item.js
@@ -1,22 +1,23 @@
 const services = document.querySelector('.services');
-const arrayOfItems = services.querySelector('.services__list').querySelectorAll('.list__item');
+const arrayOfServices = services.querySelector('.services__list').querySelectorAll('.service');
 let activeService = 1;
 
-arrayOfItems[activeService].classList.add('active');
+arrayOfServices[activeService].classList.add('active');
 
 const toggleActiveService = () => {
-  arrayOfItems.forEach(el => el.classList.remove('active'));
-  arrayOfItems[activeService].classList.add('active');
+  arrayOfServices.forEach(el => el.classList.remove('active'));
+  arrayOfServices[activeService].classList.add('active');
 };
 
 services.querySelector('.button_prev').addEventListener('click', () => {
   --activeService;
-  if (activeService < 0) activeService = arrayOfItems.length -1;
+  if (activeService < 0) activeService = arrayOfServices.length -1;
   toggleActiveService();
 });
 
 services.querySelector('.button_next').addEventListener('click', () => {
   ++activeService;
-  if (activeService > (arrayOfItems.length - 1)) activeService = 0;
+  if (activeService > (arrayOfServices.length - 1)) activeService = 0;
   toggleActiveService()
 });
+
